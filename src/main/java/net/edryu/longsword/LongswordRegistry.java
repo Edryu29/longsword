946a6c9aef65693c9ecf6b4abffeb40b049ddde1
Edryu <trickyjuggler29@gmail.com>
2024-02-24 11:44:53 -0500

    Fixed material damage

--- src/main/java/net/edryu/longsword/LongswordRegistry.java
+++ src/main/java/net/edryu/longsword/LongswordRegistry.java
@@ -16,11 +16,11 @@ public class LongswordRegistry {
 	public static final float longswordSpeed = -3.0f;
 
     public static final LongswordItem WoodenLongswordItem = registerLongsword("wooden_longsword", new LongswordItem(ToolMaterials.WOOD, longswordDamage, longswordSpeed, new Item.Settings()));
-    public static final LongswordItem StoneLongswordItem = registerLongsword("stone_longsword", new LongswordItem(ToolMaterials.WOOD, longswordDamage, longswordSpeed, new Item.Settings()));
-    public static final LongswordItem IronLongswordItem = registerLongsword("iron_longsword", new LongswordItem(ToolMaterials.WOOD, longswordDamage, longswordSpeed, new Item.Settings()));
-    public static final LongswordItem GoldenLongswordItem = registerLongsword("golden_longsword", new LongswordItem(ToolMaterials.WOOD, longswordDamage, longswordSpeed, new Item.Settings()));
-    public static final LongswordItem DiamondLongswordItem = registerLongsword("diamond_longsword", new LongswordItem(ToolMaterials.WOOD, longswordDamage, longswordSpeed, new Item.Settings()));
-    public static final LongswordItem NetheriteLongswordItem = registerLongsword("netherite_longsword", new LongswordItem(ToolMaterials.WOOD, longswordDamage, longswordSpeed, new Item.Settings()));
+    public static final LongswordItem StoneLongswordItem = registerLongsword("stone_longsword", new LongswordItem(ToolMaterials.STONE, longswordDamage, longswordSpeed, new Item.Settings()));
+    public static final LongswordItem IronLongswordItem = registerLongsword("iron_longsword", new LongswordItem(ToolMaterials.IRON, longswordDamage, longswordSpeed, new Item.Settings()));
+    public static final LongswordItem GoldenLongswordItem = registerLongsword("golden_longsword", new LongswordItem(ToolMaterials.GOLD, longswordDamage, longswordSpeed, new Item.Settings()));
+    public static final LongswordItem DiamondLongswordItem = registerLongsword("diamond_longsword", new LongswordItem(ToolMaterials.DIAMOND, longswordDamage, longswordSpeed, new Item.Settings()));
+    public static final LongswordItem NetheriteLongswordItem = registerLongsword("netherite_longsword", new LongswordItem(ToolMaterials.NETHERITE, longswordDamage, longswordSpeed, new Item.Settings()));
 
 	public static LongswordItem registerLongsword(String name, LongswordItem item) {
 		return Registry.register(Registries.ITEM, new Identifier("longsword", name), item);
@@ -28,7 +28,7 @@ public class LongswordRegistry {
 
 	public static void init() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
-			content.addAfter(Items.NETHERITE_SWORD, WoodenLongswordItem);
+			content.addAfter(Items.NETHERITE_AXE, WoodenLongswordItem);
 			content.addAfter(WoodenLongswordItem, StoneLongswordItem);
 			content.addAfter(StoneLongswordItem, IronLongswordItem);
 			content.addAfter(IronLongswordItem, GoldenLongswordItem);
